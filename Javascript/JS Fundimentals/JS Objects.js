9e8f174434d794f3507ca5dbc3a00d2a78381dbc
DanielKirkby <38848260+DanielKirkby@users.noreply.github.com>
2018-07-03 15:31:18 -0500

    update

--- Javascript/JS Fundimentals/JS Objects.js
+++ Javascript/JS Fundimentals/JS Objects.js
@@ -16,3 +16,29 @@ function namesCohorts(list) {
 }
 
 namesCohorts(students);
+
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ }; 
+
+ function empManagers(list) {
+     console.log('EMPLOYEES');     
+     for (let index = 0; index < list['employees'].length; index++) {
+        console.log(index+1+' - ' +list['employees'][index].first_name+ ', '+list['employees'][index].last_name);
+     }
+     console.log('MANAGERS');
+     for (let index = 0; index < list['managers'].length; index++) {
+        console.log(index+1+' - ' +list['managers'][index].first_name+ ', '+list['managers'][index].last_name);   
+     }
+ }
+ empManagers(users)
